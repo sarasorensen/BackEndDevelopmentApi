9391265c2eee516c88cec182393a647449f32d88
Sara <sara.sorensen@bouvet.no>
2025-08-26 15:09:15 +0200

    express

--- myApp.js
+++ myApp.js
@@ -1,1 +1,14 @@
-console.log("Hello World");
+// 1. Import Express and create the app
+const express = require('express');
+const app = express();
+
+// 2. Define the GET route at "/"
+app.get('/', function(req, res) {
+  res.send('Hello Express');
+});
+
+// 3. Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
