e9d977c16c9eec134419406b2cc4aa734adb6b1a
Sara <sara.sorensen@bouvet.no>
2025-08-27 14:58:46 +0200

    css

--- myApp.js
+++ myApp.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 
-// Serve the index.html file on GET requests to the root path
+// Serve static files from the /public folder
+app.use('/public', express.static(__dirname + '/public'));
+
+// Serve the HTML file on GET requests to the root path
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
