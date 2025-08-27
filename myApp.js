882410ff13f65c4079ad53cd0b540e0d138f1cec
Sara <sara.sorensen@bouvet.no>
2025-08-27 15:00:02 +0200

    json route

--- myApp.js
+++ myApp.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app = express();
 
-// Serve static files from the /public folder
+// Serve static files from /public
 app.use('/public', express.static(__dirname + '/public'));
 
-// Serve the HTML file on GET requests to the root path
+// Serve HTML file on the root path
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
+});
+
+// Serve JSON on /json route
+app.get('/json', function(req, res) {
+  res.json({ message: 'Hello json' });
 });
 
 module.exports = app;
