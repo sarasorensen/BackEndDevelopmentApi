63c6a292db7f6700cbcc7e7cd0d36205bdf9e1f3
Sara <sara.sorensen@bouvet.no>
2025-08-28 10:41:06 +0200

    lowercase added

--- myApp.js
+++ myApp.js
@@ -1,20 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-require('dotenv').config();
+require("dotenv").config();
 
 // Serve static files from /public
-app.use('/public', express.static(__dirname + '/public'));
+app.use("/public", express.static(__dirname + "/public"));
 
 // Serve HTML file on the root path
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 // Serve JSON on /json route
-app.get('/json', function(req, res) {
-  let message = 'Hello json';
-  if (process.env.MESSAGE_STYLE === 'uppercase') {
+app.get("/json", function (req, res) {
+  let message = "Hello json";
+  if (process.env.MESSAGE_STYLE === "uppercase") {
     message = message.toUpperCase();
+  } else {
+    message = message.toLowerCase();
   }
   res.json({ message });
 });
