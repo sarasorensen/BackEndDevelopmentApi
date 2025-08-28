05ea0ee20a85f6c30c447e69d8e5fd4e50a62d5f
Sara <sara.sorensen@bouvet.no>
2025-08-28 10:23:40 +0200

    test

--- myApp.js
+++ myApp.js
@@ -1,24 +1,15 @@
-require('dotenv').config(); // Load environment variables
 const express = require('express');
 const app = express();
 
-// Serve static files from /public
 app.use('/public', express.static(__dirname + '/public'));
 
-// Serve HTML file on the root path
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// Serve JSON on /json route, applying MESSAGE_STYLE if set
 app.get('/json', (req, res) => {
   let message = 'Hello json';
-  
-  // Check environment variable inside the handler
-  if (process.env.MESSAGE_STYLE === 'uppercase') {
-    message = message.toUpperCase();
-  }
-  
+  if (process.env.MESSAGE_STYLE === 'uppercase') message = message.toUpperCase();
   res.json({ message });
 });
 
