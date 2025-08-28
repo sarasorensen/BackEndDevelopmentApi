d685819d2fdb7fc7b53a97f1bca9635c98c4b22c
Sara <sara.sorensen@bouvet.no>
2025-08-28 10:28:27 +0200

    test2

--- myApp.js
+++ myApp.js
@@ -1,10 +1,24 @@
+const express = require('express');
+const app = express();
+
+// Serve static assets
+app.use('/public', express.static(__dirname + '/public'));
+
+// Serve HTML on the root path
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+// Serve JSON at /json
 app.get('/json', (req, res) => {
   let message = 'Hello json';
 
-  // Check the MESSAGE_STYLE environment variable inside the route handler
+  // Read MESSAGE_STYLE inside the route handler
   if (process.env.MESSAGE_STYLE === 'uppercase') {
     message = message.toUpperCase();
   }
 
   res.json({ message });
 });
+
+module.exports = app;
