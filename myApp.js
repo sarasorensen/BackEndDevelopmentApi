db3c48c35813ec786281c951fb5487d188b4fbdf
Sara <sara.sorensen@bouvet.no>
2025-09-01 14:57:35 +0200

    connection

--- myApp.js
+++ myApp.js
@@ -1,10 +1,15 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+if (process.env.MONGO_URI) {
+  mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+
+  mongoose.connection.on('connected', () => console.log('MongoDB connected'));
+  mongoose.connection.on('error', err => console.log('MongoDB connection error:', err));
+}
 
 let Person;
 
